54b8c8988b9c68357b5f401e82e621c5580d02fd
Erkin <travijuu@gmail.com>
2013-07-09 10:35:13 +0300

    ruler changed

--- pgcmt/ticket/views.py
+++ pgcmt/ticket/views.py
@@ -23,13 +23,21 @@ def slugify(value):
 def home(request):
     tickets = Ticket.objects.all().order_by("-id")
     search_form = SearchTicketForm()
-    return render_to_response(TEMPLATE_TICKET_LIST,{'tickets':tickets,'search_form':search_form,'title':'All Tickets','user':request.user,'fullView':False,'projects':getProjectList()})
+    context = {
+        'tickets':tickets,'search_form':search_form,'title':'All Tickets' 
+        ,'user':request.user,'fullView':False,'projects':getProjectList()
+    }
+    return render_to_response( TEMPLATE_TICKET_LIST, context )
 
 @login_required
 def createProject(request):
     form = CreateProjectForm(request.POST or None)
     if request.method == 'GET':
-        return render(request,TEMPLATE_PROJECT_FORM,{'form':form,'title':'Create Project','user':request.user,'projects':getProjectList()})
+        context = {
+            'form':form,'title':'Create Project','user':request.user,
+            'projects':getProjectList()
+        }
+        return render(request,TEMPLATE_PROJECT_FORM, context)
     elif request.method == 'POST':
         if form.is_valid():
             form.save()
@@ -41,33 +49,52 @@ def createProject(request):
             if request.is_ajax():
                 response = {'result':'false','message':'Form is invalid.'}
                 return HttpResponse(simplejson.dumps(response),mimetype="application/json");
-            return render(request,TEMPLATE_PROJECT_FORM,{'form':form,'title':'Create Project','user':request.user,'projects':getProjectList()})
+            context = {
+                'form':form,'title':'Create Project','user':request.user,
+                'projects':getProjectList()
+                }
+            return render_to_response( TEMPLATE_PROJECT_FORM, context )
 
 @login_required
 def createTicket(request):
     form = CreateTicketForm(request.POST or None)
     if request.method == 'GET':
-        return render(request,TEMPLATE_TICKET_FORM,{'form':form,'title':'Create Ticket','user':request.user, 'projects':getProjectList()})
+        context = {
+            'form':form,'title':'Create Ticket','user':request.user, 
+            'projects': getProjectList() 
+        }
+        return render( request, TEMPLATE_TICKET_FORM, context )
     elif request.method == 'POST':
         if form.is_valid():
             ticket = form.save(commit=False)
             ticket.user = request.user
             ticket.save();
             if request.is_ajax():
-                response = {'result':'true','message':'Ticket created.','redirect': reverse("ShowTicket",args=(str(ticket.id),) ) }
+                response = {
+                    'result':'true','message':'Ticket created.',
+                    'redirect': reverse("ShowTicket",args=(str(ticket.id),) ) 
+                }
                 return HttpResponse(simplejson.dumps(response),mimetype="application/json");
             return HttpResponseRedirect(reverse("ShowTicket",args=(str(ticket.id),)) )
         else:
             if request.is_ajax():
                 response = {'result':'false','message':'Form is invalid.'}
                 return HttpResponse(simplejson.dumps(response),mimetype="application/json");
-            return render(request,TEMPLATE_TICKET_FORM,{'form':form,'title':'Create Ticket','user':request.user,'projects':getProjectList()})    
+            context = {
+                'form':form,'title':'Create Ticket','user':request.user,
+                'projects':getProjectList()
+            }
+            return render( request, TEMPLATE_TICKET_FORM, context)
     
 @login_required
 def createRequestUser(request):
     form = CreateRequestUser(request.POST or None)
     if request.method == 'GET':
-        return render(request,TEMPLATE_REQUESTUSER_FORM,{'form':form,'username':request.user,'title':'Create Responsible','projects':getProjectList()})    
+        context = {
+            'form':form,'username':request.user,'title':'Create Responsible',
+            'projects':getProjectList()
+        }
+        return render( request, TEMPLATE_REQUESTUSER_FORM, context )
     elif request.method == 'POST':
         if form.is_valid():
             form.save()
@@ -79,62 +106,103 @@ def createRequestUser(request):
             if request.is_ajax():
                 response = {'result':'false','message':'Form is invalid.'}
                 return HttpResponse(simplejson.dumps(response),mimetype="application/json");
-            return render(request,TEMPLATE_REQUESTUSER_FORM,{'form':form,'username':request.user,'title':'Create Responsible','projects':getProjectList()})    
+            context = {
+                'form':form,'username':request.user,'title':'Create Responsible',
+                'projects':getProjectList()
+            }
+            return render( request, TEMPLATE_REQUESTUSER_FORM, context )
 
 def getProjectList():
     return Project.objects.all().order_by("name").annotate(ticket_count=Count('ticket'))
 
 def listProjects(request):
     projects = getProjectList()
-    return render_to_response(TEMPLATE_PROJECT_LIST,{'projects':projects,'title':'Project List','user':request.user})
+    context = {
+        'projects':projects,'title':'Project List','user':request.user
+    }
+    return render_to_response( TEMPLATE_PROJECT_LIST, context )
 
 def showProject(request,project_name):
     project = Project.objects.get(name=project_name)
     tickets = Ticket.objects.filter(project=project).order_by("-id")
     search_form = SearchTicketForm(initial={'project_id':project})
-    return render_to_response(TEMPLATE_TICKET_LIST,{'tickets':tickets,'search_form':search_form,'title':project_name+" Project",'user':request.user,'projects':getProjectList()})
+    context = {
+        'tickets':tickets,'search_form':search_form,'title':project_name+" Project",
+        'user':request.user,'projects':getProjectList()
+    }
+    return render_to_response( TEMPLATE_TICKET_LIST, context )
 
 def searchTicket(request):
     if len(request.GET["project_id"]) > 0:
-        tickets = Ticket.objects.filter(content__icontains=request.GET["query"],project=request.GET["project_id"]).order_by("-id")
+        tickets = Ticket.objects.filter(content__icontains=request.GET["query"], \
+                            project=request.GET["project_id"]).order_by("-id")
         project = Project.objects.get(id=request.GET["project_id"]).__str__()
     else:
-        tickets = Ticket.objects.filter(content__icontains=request.GET["query"]).order_by("-id")
+        tickets = Ticket.objects.filter(content__icontains=request.GET["query"]) \
+                                .order_by("-id")
         project = "all"
     search_form = SearchTicketForm(request.GET)
-    return render_to_response(TEMPLATE_TICKET_LIST,{'tickets':tickets,'search_form':search_form,'title':'Search for ' + request.GET["query"] + ' in ' + project,'user':request.user,'projects':getProjectList() })
+    context = {
+        'tickets':tickets,'search_form':search_form,
+        'title':'Search for ' + request.GET["query"] + ' in ' + project,
+        'user':request.user,'projects':getProjectList() 
+    }
+    return render_to_response( TEMPLATE_TICKET_LIST, context )
 
 def showUser(request,username):
-    tickets = Ticket.objects.filter(user=User.objects.filter(username=username)).order_by("-id")
-    return render_to_response(TEMPLATE_TICKET_LIST,{'tickets':tickets,'title':'Tickets done by ' + username,'user':request.user,'projects':getProjectList() })
+    tickets = Ticket.objects.filter(user=User.objects.filter(username=username)) \
+                            .order_by("-id")
+    context = {
+        'tickets':tickets,'title':'Tickets done by ' + username,'user':request.user,
+        'projects':getProjectList() 
+    }
+    return render_to_response( TEMPLATE_TICKET_LIST, context )
 
 def showRequestUser(request,requestuser_id):
     requestUser = RequestUser.objects.get(id=requestuser_id)
     tickets = Ticket.objects.filter(requested_by=requestuser_id).order_by("-id")
-    return render_to_response(TEMPLATE_TICKET_LIST,{'tickets':tickets,'title':'Tickets requested by ' + requestUser.username,'user':request.user,'projects':getProjectList() })
+    context = {
+        'tickets':tickets,'title':'Tickets requested by ' + requestUser.username,
+        'user':request.user,'projects':getProjectList() 
+    }
+    return render_to_response( TEMPLATE_TICKET_LIST, context )
 
 def showTicket(request,ticketId):
     ticket = Ticket.objects.filter(id=ticketId)
-
-    return render_to_response(TEMPLATE_TICKET_LIST,{'tickets':ticket,'title':'Ticket ','fullView':True,'user':request.user,'projects':getProjectList()})
+    context = {
+        'tickets':ticket,'title':'Ticket ','fullView':True,'user':request.user,
+        'projects':getProjectList()
+    }
+    return render_to_response( TEMPLATE_TICKET_LIST, context )
 
 def editTicket(request,ticketId):
     ticket = get_object_or_404(Ticket,id=ticketId)
     form = CreateTicketForm(request.POST or None,instance=ticket)
     if request.method == 'GET':
-        return render(request,TEMPLATE_TICKET_FORM,{'form':form,'title':'Edit Ticket','user':request.user,'projects':getProjectList(),'ticketId':ticketId})    
+        context = {
+            'form':form,'title':'Edit Ticket','user':request.user,
+            'projects':getProjectList(),'ticketId':ticketId
+        }
+        return render( request, TEMPLATE_TICKET_FORM, context)
     if request.method == 'POST':
         if form.is_valid():
             form.save()
             if request.is_ajax():
-                response = {'result':'true','message':'Ticket has changed.','redirect':reverse("ShowTicket",args=(ticketId,))}
+                response = {
+                    'result':'true','message':'Ticket has changed.',
+                    'redirect':reverse("ShowTicket",args=(ticketId,))
+                }
                 return HttpResponse(simplejson.dumps(response),mimetype="application/json");
             return HttpResponseRedirect( reverse("ShowTicket",args=(ticketId,)) )
         else:
             if request.is_ajax():
                 response = {'result':'false','message':'Form is invalid.'}
                 return HttpResponse(simplejson.dumps(response),mimetype="application/json");
-            return render(request,TEMPLATE_TICKET_FORM,{'form':form,'title':'Edit Ticket','user':request.user,'projects':getProjectList(),'ticketId':ticketId})       
+            context = {
+                'form':form,'title':'Edit Ticket','user':request.user,
+                'projects':getProjectList(),'ticketId':ticketId
+            }
+            return render( request, TEMPLATE_TICKET_FORM, context )
 
 def deleteTicket(request,ticketId):
     ticket = get_object_or_404(Ticket,id=ticketId)
@@ -145,7 +213,11 @@ def deleteTicket(request,ticketId):
             response = { 'result':'true','message':'Ticket has deleted!' }
             return HttpResponse(simplejson.dumps(response),mimetype="application/json");
     else:
-        return render(request,TEMPLATE_TICKET_FORM,{'title':'Edit Ticket','user':request.user,'projects':getProjectList(),'ticketId':ticketId})       
+        context = {
+            'title':'Edit Ticket','user':request.user,'projects':getProjectList(),
+            'ticketId':ticketId
+        }
+        return render( request, TEMPLATE_TICKET_FORM, context )
 
 
 
