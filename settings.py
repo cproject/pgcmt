deb6015a2f27a70a8697b4bd4a3948e677ee78f8
Erkin Çakar <erkin@positive.com.tr>
2015-11-23 23:09:38 +0200

    jenkins

--- settings.py
+++ settings.py
@@ -16,8 +16,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'pgcmt',                      # Or path to database file if using sqlite3.
-        'USER': 'postgres',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'USER': 'jenkins',                      # Not used with sqlite3.
+        'PASSWORD': 'jenkins',                  # Not used with sqlite3.
         'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
     }
